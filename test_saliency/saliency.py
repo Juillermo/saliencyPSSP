28365ac07d8748d6d22c74e06166f8df46d81113
juillermo <grm992@gmail.com>
2018-08-06 14:18:39 +0100

    fixed batches over training set

--- test_saliency/saliency.py
+++ test_saliency/saliency.py
@@ -2,6 +2,9 @@ import pickle
 import time
 import argparse
 import importlib
+import glob
+import os
+import re
 
 import numpy as np
 import pandas as pd
@@ -13,6 +16,8 @@ import lasagne as nn
 
 from utils import decode, ssConvertMap, ssConvertString, convertPredictQ8Result2HumanReadable
 from data_managing import load_data
+
+batch_size = 64
 
 
 def spot_best(X_am, X_pssm, labels):
@@ -152,40 +157,20 @@ def jacobian(expression, wrt, consider_constant=None,
     return theano.gradient.format_as(using_list, using_tuple, jacobs)
 
 
-def compute_tensor_saliency(X_am, X_pssm, args):
-    if X_am.ndim == 2:
-        X_am = X_am[None, ...]
-        X_pssm = X_pssm[None, ...]
+def compute_tensor_jurtz(X_batch, mask_batch, batch, label, ini=0):
+    """
+    Computes the saliencies of a batch for a certain label. If ini!=0, it also repairs the ini sequence and
+    computes the rest of the batch.
 
-    model = load_model("modelQ8.h5")
+    Inputs:
+        X_batch: batch of length 64, with its corresponding mask batch
+        batch: batch number (absolute, for labeling)
+        label: one of the 8 classes (see ssConvertString)
+        ini: which sequence of the batch to start from. If not 0, it will also split the first sequence in 2
 
-    gradients = jacobian(model.outputs[0][:, :, ssConvertString.find(args.label)].flatten(),
-                         wrt=[model.inputs[0], model.inputs[1]])
-    get_gradients = K.function(inputs=[model.inputs[0], model.inputs[1], K.learning_phase()],
-                               outputs=gradients)
-    grads = get_gradients([X_am, X_pssm, 0])
+    Outputs:
+        Saves individual saliencies in separated pickle files, properly labelled"""
 
-    with open("saliencies/saliencies" + str(args.seq) + str(args.label) + ".pkl", 'wb') as f:
-        pickle.dump(grads, f, protocol=2)
-
-
-def main_saliencies():
-    parser = argparse.ArgumentParser(description='Compute saliencies')
-    parser.add_argument('--label', type=str, default='H', metavar='label',
-                        help='class to which gradients are computed (default H)')
-    parser.add_argument('--seq', type=int, default=0, metavar='seq',
-                        help='sequence of which the gradient is calculated (default 0)')
-    args = parser.parse_args()
-
-    if args.seq is not None:
-        first_seq = args.seq
-        num_seqs = 1
-
-        X_am, X_pssm, mask, labels = load_data("", first_seq=first_seq, num_seqs=num_seqs)
-        compute_tensor_saliency(X_am, X_pssm, args)
-
-
-def compute_tensor_jurtz(X, mask, args):
     metadata_path = "dump_pureConv-20180804-010835-47.pkl"
     metadata = np.load(metadata_path)
     config_name = metadata['config_name']
@@ -200,20 +185,49 @@ def compute_tensor_jurtz(X, mask, args):
     print("Load parameters")
     nn.layers.set_all_param_values(l_out, metadata['param_values'])
 
-    print("Compute saliencies")
-    for batch_seq in range(64):
-        seq_len = int(np.sum(mask[batch_seq]))
-        gradients = theano.gradient.jacobian(inference[batch_seq, :seq_len, ssConvertString.find(args.label)],
+    if ini != 0:
+        print("Computing broken saliency")
+        seq_len = int(np.sum(mask_batch[ini]))
+
+        # FIRST HALF
+        gradients = theano.gradient.jacobian(inference[ini, :seq_len // 2, ssConvertString.find(label)],
                                              wrt=sym_x)
         get_gradients = theano.function(inputs=[sym_x], outputs=gradients)
 
-        grads = get_gradients(X)
+        grads = np.array(get_gradients(X_batch))
+
+        del get_gradients
+        del gradients
+
+        # SECOND HALF
+        gradients = theano.gradient.jacobian(inference[ini, seq_len // 2:seq_len, ssConvertString.find(label)],
+                                             wrt=sym_x)
+        get_gradients = theano.function(inputs=[sym_x], outputs=gradients)
+
+        grads2 = np.array(get_gradients(X_batch))
+
+        tot_grads = np.concatenate((grads[:, ini, seq_len], grads2[:, ini, seq_len]), axis=0)
+
+        with open("saliencies_jurtz/saliencies{:4d}{:s}.pkl".format(batch_size * batch + ini, label),
+                  'wb') as f:
+            pickle.dump(tot_grads, f, protocol=2)
+
+        ini += 1
+
+    print("Compute saliencies")
+    for batch_seq in range(ini, batch_size):
+        seq_len = int(np.sum(mask_batch[batch_seq]))
+        gradients = theano.gradient.jacobian(inference[batch_seq, :seq_len, ssConvertString.find(label)],
+                                             wrt=sym_x)
+        get_gradients = theano.function(inputs=[sym_x], outputs=gradients)
+
+        grads = get_gradients(X_batch)
         grads = np.array(grads)
 
-        with open("saliencies_jurtz/saliencies" + str(64 * args.batch + batch_seq) + str(args.label) + ".pkl",
+        with open("saliencies_jurtz/saliencies{:4d}{:s}.pkl".format(batch_size * batch + batch_seq, label),
                   'wb') as f:
             pickle.dump(grads[:seq_len, batch_seq, :seq_len], f, protocol=2)
-            print(batch_seq)
+        print(batch_seq)
 
 
 def main_saliencies_jurtz():
@@ -231,15 +245,18 @@ def main_saliencies_jurtz():
             TRAIN_PATH = 'cullpdb+profile_6133_filtered.npy.gz'
             if args.batch < 5278 // batch_size:
                 X, _, _, _, mask, _, _ = get_train(TRAIN_PATH)
+                batch = args.batch
             else:
                 _, X, _, _, _, mask, _ = get_train(TRAIN_PATH)
+                batch = args.batch - 5278 // batch_size
         else:
             from data import get_test
             TEST_PATH = 'data/cb513+profile_split1.npy.gz'
             X, mask, _, _ = get_test(TEST_PATH)
+            batch = args.batch - 5534 // batch_size
 
-        idx = range(args.batch * batch_size, (args.batch + 1) * batch_size)
-        compute_tensor_jurtz(X[idx], mask[idx], args)
+        idx = range(batch * batch_size, (args.batch + 1) * batch_size)
+        compute_tensor_jurtz(X[idx], mask[idx], args.batch, args.label)
 
 
 def calculate_SeqLogo(args):
@@ -289,7 +306,82 @@ def main_SeqLogo():
         calculate_SeqLogo(args)
 
 
+def repare_saliencies():
+    origin = os.getcwd()
+    os.chdir('/scratch/grm1g17/saliencies')
+    files = glob.glob('saliencies*')
+    exists = [False for _ in range(6018)]
+    for el in files:
+        num = int(re.search(r'\d+', el).group(0))
+        exists[num] = True
+
+    os.chdir(origin)
+    from data import get_train, get_test
+    TRAIN_PATH = 'cullpdb+profile_6133_filtered.npy.gz'
+    TEST_PATH = 'cb513+profile_split1.npy.gz'
+    X, _, _, _, mask, _, _ = get_train(TRAIN_PATH)
+
+    def get_index(batch):
+        return batch
+
+    for batch in range(6018 // batch_size):
+        if batch == 5278 // batch_size:
+            _, X, _, _, _, mask, _ = get_train(TRAIN_PATH)
+
+            def get_index(batch):
+                return batch - 5278 // batch_size
+
+        elif batch == 5534 // batch_size:
+            X, mask, _, _ = get_test(TEST_PATH)
+
+            def get_index(batch):
+                return batch - 5534 // batch_size
+
+        for batch_seq in range(batch_size):
+            seq = batch * batch_size + batch_seq
+            if not exists[seq]:
+                idx = get_index(batch)
+                for label in ssConvertString:
+                    compute_tensor_jurtz(X[idx], mask[idx], batch, label, ini=batch_seq)
+                break
+
+
 if __name__ == "__main__":
     # main_saliencies()
     # main_SeqLogo()
     main_saliencies_jurtz()
+    # repair_saliencies()
+
+
+# DEPRECATED
+def compute_tensor_saliency(X_am, X_pssm, args):
+    if X_am.ndim == 2:
+        X_am = X_am[None, ...]
+        X_pssm = X_pssm[None, ...]
+
+    model = load_model("modelQ8.h5")
+
+    gradients = jacobian(model.outputs[0][:, :, ssConvertString.find(args.label)].flatten(),
+                         wrt=[model.inputs[0], model.inputs[1]])
+    get_gradients = K.function(inputs=[model.inputs[0], model.inputs[1], K.learning_phase()],
+                               outputs=gradients)
+    grads = get_gradients([X_am, X_pssm, 0])
+
+    with open("saliencies/saliencies" + str(args.seq) + str(args.label) + ".pkl", 'wb') as f:
+        pickle.dump(grads, f, protocol=2)
+
+
+def main_saliencies():
+    parser = argparse.ArgumentParser(description='Compute saliencies')
+    parser.add_argument('--label', type=str, default='H', metavar='label',
+                        help='class to which gradients are computed (default H)')
+    parser.add_argument('--seq', type=int, default=0, metavar='seq',
+                        help='sequence of which the gradient is calculated (default 0)')
+    args = parser.parse_args()
+
+    if args.seq is not None:
+        first_seq = args.seq
+        num_seqs = 1
+
+        X_am, X_pssm, mask, labels = load_data("", first_seq=first_seq, num_seqs=num_seqs)
+        compute_tensor_saliency(X_am, X_pssm, args)
